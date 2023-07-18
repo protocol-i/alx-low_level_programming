60410cb1d37e1ef8563288d8cbc96f71f40ae713
protocol-i <protocoliddriss@gmail.com>
2023-07-18 15:35:59 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main Entry
+ * Return 0 - always
+ */
+int main(void){
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
