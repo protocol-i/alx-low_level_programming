0f3e43c0e286e7b2940912dc2f43b93a21b96a62
protocol-i <protocoliddriss@gmail.com>
2023-07-18 16:02:17 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - print _putchar
- * Is written by zak
  * Return: 0
  */
 
 int main(void)
 {
 _putchar('_');
-_putchar('P');
+_putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
