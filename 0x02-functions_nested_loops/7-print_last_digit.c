133107a9935628b8154e4504f890d242d278c803
protocol-i <protocoliddriss@gmail.com>
2023-07-18 22:13:38 -0700

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
+ * @k: the int to extract the last digit from
  * Return: value of the last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int k)
 {
 	int a;	
 
-	if (n < 0)
-		n = -n;
+	if (k < 0)
+		k = -k;
 
-	a = n % 10;
+	a = k % 10;
 		
 	if (a < 0)
 		a = -a;
