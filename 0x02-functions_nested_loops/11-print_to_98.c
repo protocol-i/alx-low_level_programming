9df2cd9ccbf4c4a30a7dcd763cef66cdd4554c0c
protocol-i <protocoliddriss@gmail.com>
2023-07-18 22:51:17 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-	#include "main.h"
-
-
-	/**
-	 * print_to_98 - prints all natural numbers from n to 98,
-	 * followed by a new line
-	 * @n: print from this number
-	 */
-	void print_to_98(int n)
-	{
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
+void print_to_98(int n)
+{
 		int i, j;
 
 
@@ -30,5 +28,5 @@
 				else if (j == 98)
 					printf("%d\n", j);
 			}
-		}
 	}
+}
