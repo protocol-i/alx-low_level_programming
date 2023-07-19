31e588534e3951c17f9103efa8722d656c9d4d2a
protocol-i <protocoliddriss@gmail.com>
2023-07-18 23:27:11 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
+ * main - Test function for positive or negative
+ * Return: 0
+ */
 int main(void)
 {
-int r;
-r = 0;
-positive_or_negative(r);
-return (0);
+	int r;
+	r = 0;
+	positive_or_negative(r);
+	return (0);
 }
