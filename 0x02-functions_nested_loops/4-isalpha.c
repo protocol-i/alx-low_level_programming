004cf494e2ddbeac4525b883a0e8c00c6b46f612
protocol-i <protocoliddriss@gmail.com>
2023-07-18 17:55:28 -0700

    done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+	/**
+	 * _isalpha - checks for alphabetic character
+	 * @k: the character to be checked
+	 * Return: 1 if k is a letter, 0 otherwise
+	 */
+	int _isalpha(int k)
+	{
+		return ((k >= 'a' && k <= 'z') || (k >= 'A' && k <= 'Z'));
+	}
