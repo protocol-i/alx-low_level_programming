f27bda66102898d29121e64fd9411b3776bf13bb
protocol-i <protocoliddriss@gmail.com>
2023-07-18 22:53:15 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
-
-	/**
-	 * print_times_table - prints the n times table, starting with 0
-	 * @n: number of the times table
-	 */
-	void print_times_table(int n)
-	{
-		int i, j, k;
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
 
 
 		if (n >= 0 && n <= 15)
