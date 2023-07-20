27ebb154a32395c6582740328fe69251a875e966
protocol-i <protocoliddriss@gmail.com>
2023-07-20 07:14:09 -0700

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplys 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: addition of the multiplicattion
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
