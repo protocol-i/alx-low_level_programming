2a4e9e10a1877bbec567509f5900182b347ec587
protocol-i <protocoliddriss@gmail.com>
2023-07-20 07:28:12 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
- * print_numbers - prints numbers from 0 to 9
- * Returns: void
-*/
+ * print_numbers - print 0-9
+ * return: void
+ */
+
 void print_numbers(void)
 {
 	char c;
 
-	for (c = 0;c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
