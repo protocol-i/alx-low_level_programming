e0aac9874e09ad4c614936840566f87ba4912179
protocol-i <protocoliddriss@gmail.com>
2023-07-21 10:28:52 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
- * Returns: 0
+ * main - prints the numbers from 1 to 100 and Fizz Buzz
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
