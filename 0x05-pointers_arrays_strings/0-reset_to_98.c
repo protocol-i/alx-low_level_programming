fcb300f3eb99561ed0d08e1b35e56e31c6d599ed
protocol-i <protocoliddriss@gmail.com>
2023-07-24 03:00:41 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @p: input
+ * Returns: p
+*/
+void reset_to_98(int *p)
+{
+	*p = 98;
+}
