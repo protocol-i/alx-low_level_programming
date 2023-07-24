6a6bb85ac3f6a250a0d60a9f991a310d2c5c2d2e
protocol-i <protocoliddriss@gmail.com>
2023-07-24 08:04:16 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int y = 0;
+
+	while (*(src + l) != '\0')
+	{
+		i++;
+	}
+	for ( ; y < i ; y++)
+	{
+		dest[y] = src[y];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
