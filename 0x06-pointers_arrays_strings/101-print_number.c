2efa3eb002f2d6cedf0b2f9cc31ca587cb93ceaf
protocol-i <protocoliddriss@gmail.com>
2023-07-26 11:42:46 -0700

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,11 +7,12 @@
 void print_number(int n)
 {
 	unsigned int n1;
+
 	n1 = -n;
 }
-if(n1/10 != 0)
+	if (n1 / 10 != 0)
 {
-	print_number(n1/10);
+	print_number(n1 / 10);
 }
-_putchar((n1%10) + '0');
+	_putchar((n1 % 10) + '0');
 }
