ce851a59257d513428892a1cb7e0bb005fa89b4d
protocol-i <protocoliddriss@gmail.com>
2023-07-26 05:08:12 -0700

    7. Mozart composed his music not for the elite, but for everybody

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode into 1337 speak
+ * @n : input value
+ * Return : n value
+*/
+char*leet(char*n)
+{
+	int m,j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (m = 0; n[m] != '\0'; m++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[m] == s1[j])
+			{
+				n[m] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
