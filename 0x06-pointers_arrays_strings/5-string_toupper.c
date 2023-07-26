165c481a64b0b5724544730bfcf5b048a1ce69da
protocol-i <protocoliddriss@gmail.com>
2023-07-26 04:41:37 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_touppe - changes all lowercase to uppercase
+ * @n : pointer
+ * Return : n
+*/
+char*string_toupper(char*n)
+{
+	int m;
+
+	m = 0;
+	while (n[m] != '\0')
+	{
+		if (n[m] >= 'a' && n[m] <= 'z')
+			n[m] = n[m] - 32;
+		m++;
+	}
+	return (n);
+}
