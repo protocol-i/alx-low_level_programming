0958c79af7720bd5c0c4b532042574df52fca1ba
protocol-i <protocoliddriss@gmail.com>
2023-07-26 10:53:11 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,9 +4,9 @@
  * @dest : input value
  * @src : input value
  * @n : input value
- * Return : dest
+ * Return: dest
 */
-char*_strncpy(char*dest, char*src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
