c0cf4022d383d725e1b9f5c20b784c12643d5e32
protocol-i <protocoliddriss@gmail.com>
2023-07-26 05:33:48 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string parameters
+ * Return : *s
+*/
+char*rot13(char*s)
+{
+	int m;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRLSUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[m] != '\0'; m++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[m] == data1[j])
+			{
+				s[m] = datarot[j];
+
+				break;
+			}
+		}
+	}
+	return (s);
+}
