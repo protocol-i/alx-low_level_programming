66fa57d6d806acd15fb3d3e0a27126678188dd9a
protocol-i <protocoliddriss@gmail.com>
2023-07-26 06:44:23 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints a[2] = 98;
+ * Return : 0;
+*/ 
+int main(void)
+{
+	int m;
+	int r[5];
+	int *p;
+
+	r[2] = 1024;
+	p = &n;
+	/**
+	 * write your line of code here...
+	 * * Remember:
+	 * * - you are not allowed to use a
+	 * * - you are not allowed to modify p
+	 * * - only one statement
+	 * * - you are not allowed to code anything else than this line of code
+	 */
+       	/* ...so that this prints 98\n */
+	(p + 5) = 98;
+	print("r[2] = %d\n", a[2]);
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,74 @@
+#include "main.h"
+/**
+ * rev_string - reverse array
+ * @n : integer parameters
+ * Return: 0
+*/
+void rev_string(char*n)
+{
+	int m = 0;
+	int j = 0;
+	char t;
+
+	while(*(n + m) !=  '\0')
+	{
+		m++;
+	}
+
+	for (j = 0; j < m; j++, m--)
+	{
+		t = *(n + j);
+		*(n + j) = *(n + j);
+		*(n + m) = t;
+	}
+}
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
+char*infinte_add(char*n1, char*n2, char*r, int size_r)
+{
+	int overflow = 0, m = 0, numbers = 0;
+	int val1 = 0, val2 = 0, temp_t =0;
+
+	while(*(n1 + m) != '\0')
+		m++;
+	m--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (m < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + m) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		tempt_t = val1 + val2 + overflow;
+		if(temp_t >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (numbers >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		numbers++;
+		j--;
+		m--;
+	}
+
+	if (digits == size_r)
+		return (0);
+	*(r + numbers) = '\0';
+	rev_string(r);
+	return (r);
+}
+
