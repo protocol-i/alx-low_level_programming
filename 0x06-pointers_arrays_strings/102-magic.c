3ed08e92cfd53ab63d344715a5e3be392a6e1091
protocol-i <protocoliddriss@gmail.com>
2023-07-27 02:59:06 -0700

    10. A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - program prints a[2] = 98
+ * Return: 0
+*/
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &n;
+	a[2] = 1024;
+	p = &n;
   /*
    * writes your line of code here...
    * Remember:
@@ -16,8 +19,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+	*(p + 5) = 98;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
