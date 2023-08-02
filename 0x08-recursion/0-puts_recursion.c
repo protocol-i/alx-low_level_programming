28126085787cbafb6f7ae3e70b8b90b9a482f8f9
protocol-i <protocoliddriss@gmail.com>
2023-08-02 02:13:45 -0700

    First commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
-else
-
-_putchar('\n');
+	else
+		_putchar('\n');
 }
