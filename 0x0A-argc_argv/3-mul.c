95534df98860a1b4ac558641aa7397400bf7a5c3
protocol-i <protocoliddriss@gmail.com>
2023-08-03 17:19:04 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,7 +15,6 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
-
 	while (s[len] != '\0')
 		len++;
 	while (i < len && f == 0)
@@ -23,7 +22,7 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 		if (s[i] >= '0' && s[i] <= '9')
-		{
+		}
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
@@ -38,4 +37,25 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
+}
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
