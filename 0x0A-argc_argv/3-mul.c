6b977bf41fb1865aee43d7157f712216df138066
protocol-i <protocoliddriss@gmail.com>
2023-08-06 04:00:15 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,6 +4,7 @@
 
 /**
  * main - Prints the multiplication of two args number
+ * _atoi - converts a string to an integer
  * @argc: arugument counter
  * @argv: argument vector
  * Return: 0
@@ -14,8 +15,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
+		n1 = _atoi(argv[1]);
+		n2 = _atoi(argv[2]);
 		printf("%d\n", n1 * n2);
 	}
 	else
