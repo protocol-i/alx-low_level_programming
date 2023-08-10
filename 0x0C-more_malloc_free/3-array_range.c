8562697cc02ac7fb3d8a0fcc7f6ca9f703c4f5fc
protocol-i <protocoliddriss@gmail.com>
2023-08-10 09:01:10 -0700

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,26 +1,30 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * *array_range - creates an array of integers
  * @min: minimum range of values stored
- * @max: maximum range of values stored 
+ * @max: maximum range of values stored and number of elements
+ *
  * Return: pointer to the new array
  */
+int *array_range(int min, int max)
 {
 	int *ptr;
-	int m, magnitude;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
-	magnitude = max - min + 1;
 
-	ptr = malloc(sizeof(int) *magnitude);
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	for (m = 0; min <= max; m++)
-		ptr[m] = min++;
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
 
 	return (ptr);
 }
