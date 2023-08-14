d1f09e1346b530d56f030152b80dbe353030fda5
protocol-i <protocoliddriss@gmail.com>
2023-08-14 06:24:35 -0700

    2. File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+    printf("This program was compiled from the file: %s\n", __FILE__);
+    return 0;
+}
