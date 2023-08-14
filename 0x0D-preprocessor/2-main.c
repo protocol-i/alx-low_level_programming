b1a2918b3a4d7ce437c49065d1509297aa2d905b
protocol-i <protocoliddriss@gmail.com>
2023-08-14 06:27:08 -0700

    3. Function-like macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-int main() {
-    printf("This program was compiled from the file: %s\n", __FILE__);
-    return 0;
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
