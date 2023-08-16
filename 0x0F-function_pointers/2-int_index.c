2fd8acbd2a34b468a96cfcb2cdeef0bf97e0ebef
protocol-i <protocoliddriss@gmail.com>
2023-08-16 05:16:21 -0700

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - returns the index place if comparison is true else -1
+ * @array: array
+ * @size: size of elements in the array
+ * @cmp: pointer to function of one of the 3 in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (a = 0; a < size; a++)
+	{
+		if (cmp(array[a]))
+			return (a);
+	}
+	return (-1);
+}
