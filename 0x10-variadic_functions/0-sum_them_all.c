39c0ad1c78ac2a16c7b4fd1598c3ad23009f35c9
protocol-i <protocoliddriss@gmail.com>
2023-08-17 07:06:50 -0700

    0. Beauty is variable, ugliness is constant updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,30 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-/**
- * sum_them_all - Returns the sum of all parameters
- * @n: the number of parameters passed to the function
- * @...: variadic number of parameters to calculate the sum of parameters
- * return: 0
- */
-int sum_them_all(const unsigned int n, ...)
-{
-	va_list ap;
-	unsigned int m, sum = 0;
 
-	va_start(ap, n);
 
-	for (m = 0; m < n; m++)
-		sum += va_arg(ap, int);
+	/**
+	 * sum_them_all - Returns the sum of all its paramters.
+	 * @n: The number of paramters passed to the function.
+	 * @...: A variable number of paramters to calculate the sum of.
+	 *
+	 * Return: If n == 0 - 0.
+	 *         Otherwise - the sum of all parameters.
+	 */
+	int sum_them_all(const unsigned int n, ...)
+	{
+		va_list ap;
+		unsigned int j, sum = 0;
 
-	va_end(ap);
 
-	return (sum);
-}
+		va_start(ap, n);
+
+
+		for (j = 0; j < n; j++)
+			sum += va_arg(ap, int);
+
+
+		va_end(ap);
+
+
+		return (sum);
+	}
