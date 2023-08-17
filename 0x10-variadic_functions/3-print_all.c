e67a577e679fdeebdd06aee20cdf2c35c720f4cb
protocol-i <protocoliddriss@gmail.com>
2023-08-17 08:05:52 -0700

    3. To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	int j = 0;
+	char *strg, *sep = "";
+
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[j])
+		{
+			switch (format[j])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					strg = va_arg(list, char *);
+					if (!strg)
+						strg = "(nil)";
+					printf("%s%s", sep, strg);
+					break;
+				default:
+					j++;
+					continue;
+			}
+			sep = ", ";
+			j++;
+		}
+	}
+
+	printf("\n");
+	va_end(list);
+}
