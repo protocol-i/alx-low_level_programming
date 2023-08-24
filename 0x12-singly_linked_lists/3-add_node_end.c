02fce964ad2e49b6574154174f47873e256b6c5f
protocol-i <protocoliddriss@gmail.com>
2023-08-24 13:01:04 -0700

    3. Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,36 +11,31 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	if (new_node == NULL)
+	while (str[len])
+		len++;
 
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-
-		return (NULL);
-	}
-
-	new_node->next = NULL;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new_node;
+		*head = new;
+		return (new);
 	}
-	else
-	{
-		list_t *current = *head;
 
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-		current->next = new_node;
-	}
-	return (new_node);
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+
+	return (new);
 }
+
