fe586c55eed4ed63af3d487bd75c69bdd0247adc
protocol-i <protocoliddriss@gmail.com>
2023-08-24 09:50:16 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,38 +1,24 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
+
 /**
- * print_list - Prints the elements of a linked list.
- * @h: A pointer to the head of the list.
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
  *
- * Return: The number of nodes in the list.
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
-		{
-			size_t len = 0;
-
-			while (h->str[len])
-				len++;
-			for (size_t i = 0; i < len; i++)
-				_putchar(h->str[i]);
-		}
+			_putchar("[0] (nil)\n");
 		else
-		{
-			char nil_msg[] = "[0] (nil)";
-			for (size_t i = 0; i < sizeof(nil_msg) - 1; i++)
-
-			_putchar(nil_msg[i]);
-		}
-		_putchar('\n');
-		count++;
-
+			_putchar("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
-
-	return (count);
+	return (s);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,7 @@ typedef struct list_s {
 	char *str;
 	struct list_s *next;
 } list_t;
+int _putstr(const char *str)
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 size_t print_list(const list_t *h);
