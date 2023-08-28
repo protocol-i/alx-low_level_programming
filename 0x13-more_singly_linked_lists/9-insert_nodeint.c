dc8d85c7eb10ddc2d06ac19c565bc5e5580ce6e8
protocol-i <protocoliddriss@gmail.com>
2023-08-28 16:31:30 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,8 +4,8 @@
  * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
  * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * @index: index where the new node is added
+ * @data: data to insert in the new node
  *
  * Return: pointer to the new node, or NULL
  */
@@ -43,3 +43,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int dat
 
 	return (NULL);
 }
+
