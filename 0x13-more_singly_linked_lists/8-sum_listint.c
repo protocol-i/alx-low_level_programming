77db7cd85cf41d1a6cd2f8f6f7e6b1371f0f4a96
protocol-i <protocoliddriss@gmail.com>
2023-08-28 09:12:54 -0700

    Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - calculates the sum of all data in listint_t list
+ * @head: first node in the linked list
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int total_sum = 0;
+	listint_t *current_node = head;
+
+	while (current_node)
+	{
+		total_sum += current_node->n;
+		current_node = current_node->next;
+	}
+
+	return (total_sum);
+}
