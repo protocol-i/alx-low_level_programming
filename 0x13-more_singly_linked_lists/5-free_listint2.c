ac2827d29e642ce212b836c96adcdf1d07e3a7d9
protocol-i <protocoliddriss@gmail.com>
2023-08-28 16:24:02 -0700

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,23 @@
 #include "lists.h"
+
 /**
  * free_listint2 - frees a linked list
  * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
-	listint_t *next_nod;
+	listint_t *current_node, *next_node;
 
 	if (!head)
 		return;
 
-	current = *head;
+	current_node = *head;
 
-	while (current)
+	while (current_node)
 	{
-		next_node = current->next;
-		free(current);
-		current = next_node;
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
 	}
 
 	*head = NULL;
