767295997041b47c3ac6562e18d43f989b12222c
protocol-i <protocoliddriss@gmail.com>
2023-08-29 01:11:12 -0700

    13. Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,88 @@
 #include "lists.h"
+#include <stdio.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * free_listint_safe - frees a linked list
- * @head: pointer to the first node in the linked list
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
  *
- * Return: number of elements in the freed list
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
  */
-size_t free_listint_safe(listint_t **head)
+size_t looped_listint_len(const listint_t *head)
 {
-	size_t elements_freed = 0;
-	int diff;
-	listint_t *current;
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
 
-	if (!head || !*head)
+	if (head == NULL || head->next == NULL)
 		return (0);
 
-	while (*head)
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		diff = *head - (*head)->next;
-		if (diff > 0)
+		if (tortoise == hare)
 		{
-			current = (*head)->next;
-			free(*head);
-			*head = current;
-			elements_freed++;
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
 		}
-		else
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes, position = 0;
+
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
 		{
-			free(*head);
-			*head = NULL;
-			elements_freed++;
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
 
-	*head = NULL;
+	else
+	{
+		for (position = 0; position < nodes; position++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
 
-	return (elements_freed);
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
 }
