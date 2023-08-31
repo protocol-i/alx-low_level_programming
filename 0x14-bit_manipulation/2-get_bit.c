e52f1f0adfe4d9d5e886a3da0e56af6a36edbdc5
protocol-i <protocoliddriss@gmail.com>
2023-08-31 10:07:22 -0700

    10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given index to 1
- * @n: pointer to the number to change
- * @index: index of the bit to set to 1
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
  *
- * Return: 1 for success, -1 for failure
+ * Return: value of the bit
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
+	int bit_val;
+
 	if (index > 63)
 		return (-1);
 
-	*n = ((1UL << index) | *n);
-	return (1);
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
