ddc9f68d56ced5531d2605b4a8839ccee30d30b1
protocol-i <protocoliddriss@gmail.com>
2023-09-04 13:30:29 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,33 @@
 #include "main.h"
+
 /**
- * create_file - Creates a file with the given filename and writes text_content to it.
- * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write to the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-int create_file(const char *filename, char *text_content) {
-    int fd, write_status;
-    
-    if (filename == NULL)
-        return -1;
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600); // rw-------
-    if (fd == -1)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL) {
-        write_status = write(fd, text_content, strlen(text_content));
-        if (write_status == -1) {
-            close(fd);
-            return -1;
-        }
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    close(fd);
-    return 1;
-}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-int main(void) {
-    const char *filename = "example.txt"; // Replace with your desired filename
-    char *text_content = "This is a sample text.\n"; // Replace with your desired text content
+	if (fd == -1 || w == -1)
+		return (-1);
 
-    int result = create_file(filename, text_content);
-    if (result == -1) {
-        fprintf(stderr, "Error: Unable to create or write to the file.\n");
-        return EXIT_FAILURE;
-    }
+	close(fd);
 
-    printf("File created and written successfully.\n");
-    return EXIT_SUCCESS;
+	return (1);
 }
